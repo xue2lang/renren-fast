c6bc55984b8cf8b7f3b885851f43ec49fb36efce
sunwei <sunwei126@163.com>
2018-04-17 23:00:52 +0800

    测试项目

---
+++ src/main/java/io/renren/config/FilterConfig.java
@@ -0,0 +1,22 @@
+package io.renren.config;
+
+/**
+ * Filter配置
+ *
+ * @author chenshun
+ * @email sunlightcs@gmail.com
+ * @date 2017-04-21 21:56
+ */
+public class FilterConfig {
+
+//    @Bean
+//    public FilterRegistrationBean xssFilterRegistration() {
+//        FilterRegistrationBean registration = new FilterRegistrationBean();
+//        registration.setDispatcherTypes(DispatcherType.REQUEST);
+//        registration.setFilter(new XssFilter());
+//        registration.addUrlPatterns("/*");
+//        registration.setName("xssFilter");
+//        registration.setOrder(Integer.MAX_VALUE);
+//        return registration;
+//    }
+}
---
+++ src/test/java/io/renren/RenrenApplicationTests.java
@@ -0,0 +1,21 @@
+package io.renren;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RenrenApplicationTests {
+
+	@Test
+	public void contextLoads() {
+//		SysUserEntity user = new SysUserEntity();
+//		user.setEmail("qqq@qq.com");
+//		redisUtils.set("user", user);
+//
+//		System.out.println(ToStringBuilder.reflectionToString(redisUtils.get("user", SysUserEntity.class)));
+	}
+
+}
